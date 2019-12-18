f3b74f4e86893d4441c723a20c9db70f927e1dce
Eric murray <ericwmurray1994@gmail.com>
2019-12-18 04:19:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == "?".upcase
     return "NO, NOT SINCE 1938!"
   else phrase == ""
     return "HUH?! SPEAK UP, SONNY!"
